411703941d4c80b3a149263e4064073c2c965982
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2023-09-28 19:48:24 -0500

    explain env vars

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="vite/client" />
 
+/**
+ * The electron-vite plugin exposes environment variables from .env file
+ * to the main, preload, and renderer processes based on prefixes.
+ * The default prefixes are `MAIN_VITE_`, `RENDERER_VITE_`, and `PRELOAD_VITE_`.
+ *
+ * You access the variables in code via `import.meta.env.{key}`.
+ * For typescript support, define the keys in this interface.
+ *
+ * https://electron-vite.org/guide/env-and-mode.html
+ */
 interface ImportMetaEnv {
   /**
    * MAIN
