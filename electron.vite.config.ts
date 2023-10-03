2fa5ac6188d4c60bfc9fd164c83b190e2dce67a4
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2023-10-03 08:20:10 -0500

    feat: include git hash in full app version

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,3 +1,4 @@
+import { execSync } from 'child_process';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import {
@@ -6,17 +7,41 @@ import {
   splitVendorChunkPlugin,
   swcPlugin,
 } from 'electron-vite';
+import { type PluginOption } from 'vite';
 
 const sentryPlugin = sentryVitePlugin({
   org: 'dragonrealms-phoenix',
   project: 'phoenix',
   telemetry: false,
   disable: process.env.VITE_PLUGIN_SENTRY_ENABLE !== 'true',
-});
+}) as PluginOption;
+
+const gitHashPlugin = (): PluginOption => {
+  return {
+    name: 'git-hash-plugin',
+    config: () => {
+      return {
+        define: {
+          'import.meta.env.MAIN_VITE_GIT_SHORT_HASH': JSON.stringify(
+            execSync('git rev-parse --short HEAD').toString().trim()
+          ),
+          'import.meta.env.MAIN_VITE_GIT_LONG_HASH': JSON.stringify(
+            execSync('git rev-parse HEAD').toString().trim()
+          ),
+        },
+      };
+    },
+  };
+};
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin(), swcPlugin(), sentryPlugin],
+    plugins: [
+      externalizeDepsPlugin(),
+      swcPlugin(),
+      gitHashPlugin(),
+      sentryPlugin,
+    ],
     build: {
       sourcemap: true,
       minify: 'esbuild',
