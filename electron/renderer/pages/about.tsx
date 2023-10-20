4f86f3f6a0a704ee3a39a7a1af535ff1917899cd
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2023-10-19 21:55:57 -0500

    chore: revert about page

--- electron/renderer/pages/about.tsx
+++ electron/renderer/pages/about.tsx
@@ -1,4 +1,3 @@
-import { EuiResizableContainer, EuiText } from '@elastic/eui';
 import Link from 'next/link';
 import { useEffect } from 'react';
 import { useLogger } from '../components/logger';
@@ -19,24 +18,6 @@ const AboutPage: React.FC = (): JSX.Element => {
       <p>
         <Link href="/">Go home</Link>
       </p>
-      <EuiResizableContainer style={{ height: '200px' }}>
-        {(EuiResizablePanel, EuiResizableButton) => (
-          <>
-            <EuiResizablePanel initialSize={50} minSize="200px" tabIndex={0}>
-              <EuiText>Quisquam blanditiis nulla</EuiText>
-            </EuiResizablePanel>
-
-            <EuiResizableButton />
-
-            <EuiResizablePanel initialSize={50} minSize="200px" tabIndex={0}>
-              <EuiText>
-                Molestias amet iste sint libero illo sunt repellat rerum
-                exercitationem.
-              </EuiText>
-            </EuiResizablePanel>
-          </>
-        )}
-      </EuiResizableContainer>
     </div>
   );
 };
