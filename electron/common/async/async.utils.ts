76b6b442869c0e90343b36c4006b4fcae2e002e8
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2023-11-05 01:54:23 -0500

    feat: run in background method and test

--- electron/common/async/async.utils.ts
+++ electron/common/async/async.utils.ts
@@ -1,3 +1,19 @@
+import { createLogger } from '../logger/logger.utils';
+
+const logger = createLogger('async:utils');
+
 export const sleep = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+/**
+ * For "fire and forget" scenarios when you don't want, or can't, await
+ * a function but it *is* async and so you want the errors logged, if any.
+ */
+export const runInBackground = (fn: () => Promise<unknown>): void => {
+  Promise.resolve(fn()).catch((error) => {
+    logger.error(`unhandled promise exception: ${error?.message}`, {
+      error,
+    });
+  });
 };
