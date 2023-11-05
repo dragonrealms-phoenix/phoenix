c8285efba0178268856f1edde5ea97de3fc00066
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2023-11-05 01:50:18 -0600

    test: to confirm logging and ipc still working

--- electron/renderer/pages/home.tsx
+++ electron/renderer/pages/home.tsx
@@ -1,10 +1,22 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import { runInBackground } from '../../common/async/async.utils';
+import { useLogger } from '../components/logger';
 
 const HomePage: React.FC = (): ReactNode => {
+  // --
+  const { logger } = useLogger('page:grid');
+  useEffect(() => {
+    runInBackground(async () => {
+      const response = await window.api.ping();
+      logger.info(response); // pong
+    });
+  }, []);
+  // --
+
   // TODO show welcome page
   //      user to select recent character, or add account, etc
   //      once select a character, then log in and load grid
-  return null;
+  return <div>Hello World</div>;
 };
 
 export default HomePage;
