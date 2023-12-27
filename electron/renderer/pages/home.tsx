edca6347e54869cb31e6bf2d268ce712ea27923c
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2023-12-26 20:34:52 -0600

    feat: stub out links to perform character actions

--- electron/renderer/pages/home.tsx
+++ electron/renderer/pages/home.tsx
@@ -1,3 +1,4 @@
+import { EuiListGroup, EuiListGroupItem, EuiPanel } from '@elastic/eui';
 import { type ReactNode, useCallback, useState } from 'react';
 import { runInBackground } from '../../common/async';
 import { equalsIgnoreCase } from '../../common/string';
@@ -26,88 +27,7 @@ const HomePage: React.FC = (): ReactNode => {
     setCharacters(await window.api.listCharacters());
   }, []);
 
-  const saveAccount = useCallback(
-    (options: { accountName: string; accountPassword: string }) => {
-      runInBackground(async () => {
-        const { accountName } = options;
-        logger.info('saving account', { accountName });
-        await window.api.saveAccount(options);
-        await listCharacters();
-      });
-    },
-    []
-  );
-
-  const removeAccount = useCallback((options: { accountName: string }) => {
-    runInBackground(async () => {
-      const { accountName } = options;
-      logger.info('removing account', { accountName });
-      if (equalsIgnoreCase(playingCharacter?.accountName, accountName)) {
-        await quitCharacter();
-      }
-      await window.api.removeAccount(options);
-      await listCharacters();
-    });
-  }, []);
-
-  const addCharacter = useCallback(
-    (options: {
-      gameCode: string;
-      accountName: string;
-      characterName: string;
-    }) => {
-      runInBackground(async () => {
-        const { characterName } = options;
-        logger.info('saving character', { characterName });
-        await window.api.saveCharacter(options);
-        await listCharacters();
-      });
-    },
-    []
-  );
-
-  const removeCharacter = useCallback(
-    (options: {
-      gameCode: string;
-      accountName: string;
-      characterName: string;
-    }) => {
-      runInBackground(async () => {
-        const { gameCode, accountName, characterName } = options;
-        logger.info('removing character', { characterName });
-        if (
-          equalsIgnoreCase(playingCharacter?.gameCode, gameCode) &&
-          equalsIgnoreCase(playingCharacter?.accountName, accountName) &&
-          equalsIgnoreCase(playingCharacter?.characterName, characterName)
-        ) {
-          await quitCharacter();
-        }
-        await window.api.removeCharacter(options);
-        await listCharacters();
-      });
-    },
-    []
-  );
-
-  const playCharacter = useCallback(
-    (options: {
-      gameCode: string;
-      accountName: string;
-      characterName: string;
-    }) => {
-      runInBackground(async () => {
-        const { gameCode, accountName, characterName } = options;
-        logger.info('playing character', { characterName });
-        await window.api.playCharacter(options);
-        setPlayingCharacter({
-          gameCode,
-          accountName,
-          characterName,
-        });
-      });
-    },
-    []
-  );
+  // TODO for each runInBackground, need to catch and display errors
 
   const quitCharacter = useCallback(async () => {
     if (playingCharacter) {
@@ -116,41 +36,217 @@ const HomePage: React.FC = (): ReactNode => {
       await window.api.sendCommand('quit');
       setPlayingCharacter(undefined);
     }
-  }, []);
+  }, [logger, playingCharacter]);
+
+  const onClickQuitCharacter = useCallback(() => {
+    runInBackground(async () => {
+      await quitCharacter();
+    });
+  }, [quitCharacter]);
+
+  const saveAccount = useCallback(
+    async (options: { accountName: string; accountPassword: string }) => {
+      const { accountName } = options;
+      logger.info('saving account', { accountName });
+      await window.api.saveAccount(options);
+      await listCharacters();
+    },
+    [logger, listCharacters]
+  );
+
+  const onClickSaveAccount = useCallback(
+    (options: { accountName: string; accountPassword: string }) => {
+      runInBackground(async () => {
+        await saveAccount(options);
+      });
+    },
+    [saveAccount]
+  );
+
+  const removeAccount = useCallback(
+    async (options: { accountName: string }) => {
+      const { accountName } = options;
+      logger.info('removing account', { accountName });
+      if (equalsIgnoreCase(playingCharacter?.accountName, accountName)) {
+        await quitCharacter();
+      }
+      await window.api.removeAccount(options);
+      await listCharacters();
+    },
+    [logger, playingCharacter, listCharacters, quitCharacter]
+  );
+
+  const onClickRemoveAccount = useCallback(
+    (options: { accountName: string }) => {
+      runInBackground(async () => {
+        await removeAccount(options);
+      });
+    },
+    [removeAccount]
+  );
+
+  const saveCharacter = useCallback(
+    async (options: {
+      gameCode: string;
+      accountName: string;
+      characterName: string;
+    }) => {
+      const { characterName } = options;
+      logger.info('adding character', { characterName });
+      await window.api.saveCharacter(options);
+      await listCharacters();
+    },
+    [logger, listCharacters]
+  );
+
+  const onClickSaveCharacter = useCallback(
+    (options: {
+      gameCode: string;
+      accountName: string;
+      characterName: string;
+    }) => {
+      runInBackground(async () => {
+        await saveCharacter(options);
+      });
+    },
+    [saveCharacter]
+  );
+
+  const removeCharacter = useCallback(
+    async (options: {
+      gameCode: string;
+      accountName: string;
+      characterName: string;
+    }) => {
+      const { gameCode, accountName, characterName } = options;
+      logger.info('removing character', { characterName });
+      if (
+        equalsIgnoreCase(playingCharacter?.gameCode, gameCode) &&
+        equalsIgnoreCase(playingCharacter?.accountName, accountName) &&
+        equalsIgnoreCase(playingCharacter?.characterName, characterName)
+      ) {
+        await quitCharacter();
+      }
+      await window.api.removeCharacter(options);
+      await listCharacters();
+    },
+    [logger, playingCharacter, listCharacters, quitCharacter]
+  );
+
+  const onClickRemoveCharacter = useCallback(
+    (options: {
+      gameCode: string;
+      accountName: string;
+      characterName: string;
+    }) => {
+      runInBackground(async () => {
+        await removeCharacter(options);
+      });
+    },
+    [removeCharacter]
+  );
+
+  const playCharacter = useCallback(
+    async (options: {
+      accountName: string;
+      characterName: string;
+      gameCode: string;
+    }) => {
+      const { accountName, characterName, gameCode } = options;
+      logger.info('playing character', { characterName });
+      await window.api.playCharacter(options);
+      setPlayingCharacter({
+        accountName,
+        characterName,
+        gameCode,
+      });
+      //--
+    },
+    [logger]
+  );
+
+  const onClickPlayCharacter = useCallback(
+    (options: {
+      accountName: string;
+      characterName: string;
+      gameCode: string;
+    }) => {
+      runInBackground(async () => {
+        await playCharacter(options);
+      });
+    },
+    [playCharacter]
+  );
+
+  const accountName = 'xxx';
+  const accountPassword = 'xxx';
+  const characterName = 'xxx';
+  const gameCode = 'xxx';
 
   return (
     <div>
-      <h2>Characters</h2>
-      <div>
-        <button
+      <EuiListGroup>
+        <EuiListGroupItem
+          label="Save Account"
           onClick={() => {
-            saveAccount({
-              accountName: 'test',
-              accountPassword: 'test',
+            onClickSaveAccount({
+              accountName,
+              accountPassword,
             });
           }}
-        >
-          Save Account
-        </button>
-        <button
+        />
+        <EuiListGroupItem
+          label="Remove Account"
           onClick={() => {
-            addCharacter({
-              gameCode: 'DR',
-              accountName: 'test',
-              characterName: 'test',
+            onClickRemoveAccount({
+              accountName,
             });
           }}
-        >
-          Add Character
-        </button>
-      </div>
+        />
+        <EuiListGroupItem
+          label="Save Character"
+          onClick={() => {
+            onClickSaveCharacter({
+              accountName,
+              characterName,
+              gameCode,
+            });
+          }}
+        />
+        <EuiListGroupItem
+          label="Remove Character"
+          onClick={() => {
+            onClickRemoveCharacter({
+              accountName,
+              characterName,
+              gameCode,
+            });
+          }}
+        />
+        <EuiListGroupItem
+          label="Play Character"
+          onClick={() => {
+            onClickPlayCharacter({
+              accountName,
+              characterName,
+              gameCode,
+            });
+          }}
+        />
+        <EuiListGroupItem
+          label="Quit Character"
+          onClick={() => {
+            onClickQuitCharacter();
+          }}
+        />
+      </EuiListGroup>
       {characters.map((character) => {
         return (
-          <div key={character.characterName}>
+          <EuiPanel key={character.characterName}>
             Game Code: {character.gameCode} <br />
             Account Name: {character.accountName} <br />
             Character Name: {character.characterName}
-          </div>
+          </EuiPanel>
         );
       })}
     </div>
