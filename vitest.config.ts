a544bf67c25bdbb2afe408d17a81df529fc70a28
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2024-02-01 00:10:10 -0600

    test: ignore mocks and tests for coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      exclude: ['**/types.ts'],
+      exclude: ['**/types.ts', '**/__mocks__/**', '**/__tests__/**'],
     },
   },
 });
