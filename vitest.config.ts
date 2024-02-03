a9a050bd0266fd86b10eccf1ab0caa15ce3d4e25
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2024-02-03 00:19:49 -0600

    test(build): silent by default

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    silent: true,
+    logHeapUsage: true,
     coverage: {
       exclude: ['**/types.ts', '**/__mocks__/**', '**/__tests__/**'],
     },
