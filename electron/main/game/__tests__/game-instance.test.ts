5eab434e721ebdd5cf5183b5863c623bf90fe5fc
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2024-02-03 00:20:03 -0600

    test: stub out game tests

---
+++ electron/main/game/__tests__/game-instance.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('game-instance', () => {
+  it.todo('todo', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ electron/main/game/__tests__/game-parser.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('game-parser', () => {
+  it.todo('todo', () => {
+    expect(true).toBe(true);
+  });
+});
