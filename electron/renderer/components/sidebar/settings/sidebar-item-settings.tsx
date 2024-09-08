ab11e08c9bc5e0a66120c32ca675a51ee38952bd
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2024-09-08 11:16:34 -0500

    chore: stub out settings panel

--- electron/renderer/components/sidebar/settings/sidebar-item-settings.tsx
+++ electron/renderer/components/sidebar/settings/sidebar-item-settings.tsx
@@ -1,9 +1,16 @@
-import { EuiPanel } from '@elastic/eui';
+import { EuiCallOut, EuiPanel, EuiSpacer } from '@elastic/eui';
 import type { ReactNode } from 'react';
 
 export const SidebarItemSettings: React.FC = (): ReactNode => {
-  // TODO
-  return <EuiPanel>Settings</EuiPanel>;
+  return (
+    <EuiPanel>
+      <EuiCallOut title="Settings" iconType="gear" size="s">
+        (todo)
+      </EuiCallOut>
+
+      <EuiSpacer size="m" />
+    </EuiPanel>
+  );
 };
 
 SidebarItemSettings.displayName = 'SidebarItemSettings';
