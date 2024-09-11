01649a7c90f4d7264953d1bdb42c55f61ea2cd23
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2024-09-10 22:33:14 -0500

    feat: game stream ids

--- electron/renderer/lib/game/game-stream-ids.ts
+++ electron/renderer/lib/game/game-stream-ids.ts
@@ -1,16 +1,98 @@
-export interface GameStreamId {
+/**
+ * The DragonRealms default game streams.
+ * When the game socket sends data, it may be tagged with a stream id.
+ * The stream id indicates which game "window" the data is intended for.
+ * User-defined scripts may also output to custom streams, which
+ * aren't represented here but which Phoenix will support.
+ */
+export enum GameStreamId {
   /**
-   * User-friendly title for the game stream.
-   * Example: 'Spells' or 'Main'.
+   * The main story window. This is where most game output goes.
+   * This content is unique in that there is no stream id for it.
    */
-  title: string;
+  MAIN = '',
+  /**
+   * When the game sends periodic experience updates.
+   */
+  EXPERIENCE = 'experience',
+  /**
+   * When the game sends periodic room updates.
+   * Usually updated when you move rooms or when people enter/leave.
+   */
+  ROOM = 'room',
+  /**
+   * Displays what spells/abilities are active and their duration.
+   */
+  SPELLS = 'percWindow',
+  /**
+   * Lists inventory in the main container on your character.
+   */
+  INVENTORY = 'inv',
+  /**
+   * For Warrior Mages and characters who have magical pets.
+   * This lets the player see what their familiar sees and does.
+   * It might also be used by Empaths when perceiving wounds.
+   */
+  FAMILIAR = 'familiar',
+  /**
+   * Messages sent and received via gweths and other ESP items.
+   */
+  THOUGHTS = 'thoughts',
+  /**
+   * Most combat messages.
+   */
+  COMBAT = 'combat',
+  /**
+   * Output when you run the `ASSESS` verb.
+   */
+  ASSESS = 'assess',
+  /**
+   * The stream id is 'logons' but in game you toggle on/off for arrivals.
+   * Only for characters who opt-in to disclose their logons.
+   */
+  ARRIVALS = 'logons',
+  /**
+   * Messages when characters die.
+   * Only for characters who opt-in to disclose their deaths.
+   */
+  DEATHS = 'deaths',
+  /**
+   * Periodic messaging from items with atmospheric effects.
+   */
+  ATMOSPHERICS = 'atmospherics',
+  /**
+   * Similar to `THOUGHTS` but for game-wide chat for beginners.
+   */
+  CHATTER = 'chatter',
+  /**
+   * No idea.
+   */
+  CONVERSATION = 'conversation',
+  /**
+   * When players send and receive whispers via `WHISPER` verb.
+   */
+  WHISPERS = 'whispers',
+  /**
+   * No idea.
+   */
+  TALK = 'talk',
+  /**
+   * When players send and receive messages via `OOC` verb.
+   */
+  OOC = 'ooc',
+  /**
+   * No idea. Maybe things that happen when a character is in a group?
+   */
+  GROUP = 'group',
+}
 
+export interface GameStreamItemInfo {
   /**
    * Unique identifier for the game stream.
    * Assigned by DragonRealms.
    * Example: 'percWindow' (spells) or '' (main).
    */
-  streamId: string;
+  streamId: GameStreamId;
 
   /**
    * Unique identifier for the stream for our purposes.
@@ -18,6 +100,12 @@ export interface GameStreamId {
    * for the main stream which is 'main' instead of empty string.
    */
   itemId: string;
+
+  /**
+   * User-friendly title for the game stream.
+   * Example: 'Spells' or 'Main'.
+   */
+  itemTitle: string;
 }
 
 /**
@@ -28,95 +116,95 @@ export interface GameStreamId {
  *
  * This array is the default set of streams that we know DragonRealms supports.
  */
-export const DefaultGameStreamIds: Array<GameStreamId> = [
+export const DefaultGameStreamItemInfos: Array<GameStreamItemInfo> = [
   {
-    title: 'Main',
-    streamId: '',
-    itemId: 'main',
+    streamId: GameStreamId.MAIN, // special case, it's an empty string
+    itemId: 'main', // special case since the stream id is empty text
+    itemTitle: 'Main',
   },
   {
-    title: 'Experience',
-    streamId: 'experience',
-    itemId: 'experience',
+    streamId: GameStreamId.EXPERIENCE,
+    itemId: GameStreamId.EXPERIENCE,
+    itemTitle: 'Experience',
   },
   {
-    title: 'Room',
-    streamId: 'room',
-    itemId: 'room',
+    streamId: GameStreamId.ROOM,
+    itemId: GameStreamId.ROOM,
+    itemTitle: 'Room',
   },
   {
-    title: 'Spells',
-    streamId: 'percWindow',
-    itemId: 'percWindow',
+    streamId: GameStreamId.SPELLS,
+    itemId: GameStreamId.SPELLS,
+    itemTitle: 'Spells',
   },
   {
-    title: 'Inventory',
-    streamId: 'inv',
-    itemId: 'inv',
+    streamId: GameStreamId.INVENTORY,
+    itemId: GameStreamId.INVENTORY,
+    itemTitle: 'Inventory',
   },
   {
-    title: 'Familiar',
-    streamId: 'familiar',
-    itemId: 'familiar',
+    streamId: GameStreamId.FAMILIAR,
+    itemId: GameStreamId.FAMILIAR,
+    itemTitle: 'Familiar',
   },
   {
-    title: 'Thoughts',
-    streamId: 'thoughts',
-    itemId: 'thoughts',
+    streamId: GameStreamId.THOUGHTS,
+    itemId: GameStreamId.THOUGHTS,
+    itemTitle: 'Thoughts',
   },
   {
-    title: 'Combat',
-    streamId: 'combat',
-    itemId: 'combat',
+    streamId: GameStreamId.COMBAT,
+    itemId: GameStreamId.COMBAT,
+    itemTitle: 'Combat',
   },
   {
-    title: 'Assess',
-    streamId: 'assess',
-    itemId: 'assess',
+    streamId: GameStreamId.ASSESS,
+    itemId: GameStreamId.ASSESS,
+    itemTitle: 'Assess',
   },
   {
-    title: 'Arrivals',
-    streamId: 'logons',
-    itemId: 'logons',
+    streamId: GameStreamId.ARRIVALS,
+    itemId: GameStreamId.ARRIVALS,
+    itemTitle: 'Arrivals',
   },
   {
-    title: 'Deaths',
-    streamId: 'deaths',
-    itemId: 'deaths',
+    streamId: GameStreamId.DEATHS,
+    itemId: GameStreamId.DEATHS,
+    itemTitle: 'Deaths',
   },
   {
-    title: 'Atmospherics',
-    streamId: 'atmospherics',
-    itemId: 'atmospherics',
+    streamId: GameStreamId.ATMOSPHERICS,
+    itemId: GameStreamId.ATMOSPHERICS,
+    itemTitle: 'Atmospherics',
   },
   {
-    title: 'Chatter',
-    streamId: 'chatter',
-    itemId: 'chatter',
+    streamId: GameStreamId.CHATTER,
+    itemId: GameStreamId.CHATTER,
+    itemTitle: 'Chatter',
   },
   {
-    title: 'Conversation',
-    streamId: 'conversation',
-    itemId: 'conversation',
+    streamId: GameStreamId.CONVERSATION,
+    itemId: GameStreamId.CONVERSATION,
+    itemTitle: 'Conversation',
   },
   {
-    title: 'Whispers',
-    streamId: 'whispers',
-    itemId: 'whispers',
+    streamId: GameStreamId.WHISPERS,
+    itemId: GameStreamId.WHISPERS,
+    itemTitle: 'Whispers',
   },
   {
-    title: 'Talk',
-    streamId: 'talk',
-    itemId: 'talk',
+    streamId: GameStreamId.TALK,
+    itemId: GameStreamId.TALK,
+    itemTitle: 'Talk',
   },
   {
-    title: 'OOC',
-    streamId: 'ooc',
-    itemId: 'ooc',
+    streamId: GameStreamId.OOC,
+    itemId: GameStreamId.OOC,
+    itemTitle: 'OOC',
   },
   {
-    title: 'Group',
-    streamId: 'group',
-    itemId: 'group',
+    streamId: GameStreamId.GROUP,
+    itemId: GameStreamId.GROUP,
+    itemTitle: 'Group',
   },
 ];
