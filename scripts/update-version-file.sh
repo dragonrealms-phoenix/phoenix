7522bae3f7a8bfc49848e7c1d583063ca97c31e1
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2025-01-23 19:40:46 -0600

    chore(build): rename dist files with new version

--- scripts/update-version-file.sh
+++ scripts/update-version-file.sh
@@ -21,4 +21,13 @@ done
 # Assign values in order of precedence: flags > env vars > defaults
 VERSION="${VERSION:-"${npm_package_version}"}"
 
+# Update version that is referenced in the code
 echo "export const VERSION = '${VERSION}';" > electron/common/version.ts
+
+# Rename dist files since they were built before the version was ticked
+for file in "$ROOT_DIR/dist"/*; do
+  if [[ $file =~ (.*)-v[0-9]+\.[0-9]+\.[0-9]+-(.*) ]]; then
+    new_file="${BASH_REMATCH[1]}-v${VERSION}-${BASH_REMATCH[2]}"
+    mv "$file" "$new_file"
+  fi
+done
