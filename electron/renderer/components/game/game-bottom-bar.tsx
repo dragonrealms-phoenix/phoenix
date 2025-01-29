82fb356603a60e9ab26261beeeec40626b838256
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2025-01-28 20:08:50 -0600

    refactor(game-command): move code to own component

--- electron/renderer/components/game/game-bottom-bar.tsx
+++ electron/renderer/components/game/game-bottom-bar.tsx
@@ -1,30 +1,9 @@
-import { EuiFieldText } from '@elastic/eui';
-import type { KeyboardEvent, KeyboardEventHandler, ReactNode } from 'react';
-import { useCallback } from 'react';
-import { isEmpty } from '../../../common/string/string.utils.js';
-import { useCommandHistory } from '../../hooks/command-history.jsx';
-import { runInBackground } from '../../lib/async/run-in-background.js';
+import type { ReactNode } from 'react';
+import { GameCommandInput } from './game-command-input.jsx';
 import { GameCompass } from './game-compass.jsx';
 import { GameRoundTime } from './game-roundtime.jsx';
 
 export const GameBottomBar: React.FC = (): ReactNode => {
-  const { input, handleKeyDown, handleOnChange } = useCommandHistory();
-
-  const onKeyDown = useCallback<KeyboardEventHandler<HTMLInputElement>>(
-    (event: KeyboardEvent<HTMLInputElement>) => {
-      // Handle any history navigation.
-      handleKeyDown(event);
-      // Handle the "Enter" key to submit command to game.
-      const command = event.currentTarget.value;
-      if (event.code === 'Enter' && !isEmpty(command)) {
-        runInBackground(async () => {
-          await window.api.sendCommand(command);
-        });
-      }
-    },
-    [handleKeyDown]
-  );
-
   return (
     <div
       css={{
@@ -38,17 +17,7 @@ export const GameBottomBar: React.FC = (): ReactNode => {
       }}
     >
       <GameRoundTime />
-      {/* TODO move to GameCommandInput */}
-      <div css={{ paddingRight: '5px', width: '100%' }}>
-        <EuiFieldText
-          value={input}
-          compressed={true}
-          fullWidth={true}
-          tabIndex={0}
-          onKeyDown={onKeyDown}
-          onChange={handleOnChange}
-        />
-      </div>
+      <GameCommandInput />
       <GameCompass />
     </div>
   );
